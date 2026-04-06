7015dc8028d8be7bbc03d07020697badba3acc7e
tmoritoki0227 <tmoritoki0227@gmail.com>
2026-04-06 19:03:41 +0900

    test

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -6,22 +6,15 @@
 // LIFF ID
 // LINE Developers コンソール > LIFF-v3チャネル > LIFFタブ で発行される
 // 形式: "数字-英数字"（例: "2009627798-VOeFmL56"）
-// v3
-// export const LIFF_ID = '2009627798-VOeFmL56'
 
-//v4
+// 家計簿用 LIFF ID
 export const LIFF_ID = '2009633869-1vMxfCBb'
 
 // モバイルオーダー用 LIFF ID
-// LINE Developers コンソール > 新しいLINEログインチャネル > LIFFタブ で発行される
 export const ORDER_LIFF_ID = '2009633869-aOYeSJTB'
 
 // API Gateway のエンドポイント URL
 // 記事③で Lambda + API Gateway を作成した後に差し替える
 // 末尾のスラッシュは不要
 // 例: "https://xxxx.execute-api.ap-northeast-1.amazonaws.com/dev"
-
-// v3
-// export const API_BASE_URL = 'https://guaj20beh1.execute-api.ap-northeast-1.amazonaws.com/dev'
-// v4
 export const API_BASE_URL = 'https://0bzjm1gjyd.execute-api.ap-northeast-1.amazonaws.com/dev'
